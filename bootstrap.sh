05904400292bf655a2a5bfe141a2f8780a2699ed
Bruce Jeaung <brucejeaung@gmail.com>
2024-12-09 14:38:25 +0800

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# step 1: 安装必要的一些系统工具
+# sudo yum install -y yum-utils
+
+# Step 2: 添加软件源信息
+# yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+# Step 3: 安装Docker
+# sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Step 4: 开启Docker服务
+# sudo service docker start
+
+HOSTNAME=""
+
+# Use getopts to parse flags
+while getopts "h:" opt; do
+  case ${opt} in
+    h)
+      HOSTNAME=$OPTARG
+      ;;
+    *)
+      echo "Usage: $0 [-h hostname]"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$HOSTNAME" ]; then
+  HOSTNAME="localhost"
+fi
+
+docker compose build --build-arg BACKEND_URL=http://$HOSTNAME:8080
+FRONTEND_ORIGIN="http://$HOSTNAME" docker compose up -d
