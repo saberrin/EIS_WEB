90e81885010d9d81a58cfd52ce0a2a09c2079732
Bruce Jeaung <brucejeaung@gmail.com>
2024-12-10 09:10:56 +0800

    prepare docker installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,45 @@
 #!/bin/sh
 
-# step 1: 安装必要的一些系统工具
-# sudo yum install -y yum-utils
+# 安装docker： https://help.aliyun.com/zh/ecs/use-cases/install-and-use-docker-on-a-linux-ecs-instance#8dca4cfa3dn0e
+# 设置docker仓库镜像：https://cloud.tencent.com/developer/article/2471124
 
-# Step 2: 添加软件源信息
-# yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+if which docker &> /dev/null
+then
+  echo "docker environment ready, skip installing"
+else
+  echo "docker is missing, start installing"
+  apt-get update
+  apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+  add-apt-repository -y "deb [arch=$(dpkg --print-architecture)] https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+  
+  echo "package source prepared"
 
-# Step 3: 安装Docker
-# sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  systemctl start docker
+  systemctl enable docker
 
-# Step 4: 开启Docker服务
-# sudo service docker start
+  echo "docker installed"
+fi
 
 HOSTNAME=""
 ORIGIN=""
+REGISTRY=""
 
 # Use getopts to parse flags
-while getopts "h:" opt; do
+while getopts "h:o:r:" opt; do
   case ${opt} in
     h)
       HOSTNAME=$OPTARG
+      ;;
+    o)
       ORIGIN=$OPTARG
       ;;
+    r)
+      REGISTRY=$OPTARG
+      ;;
     *)
-      echo "Usage: $0 [-h hostname]"
+      echo "Usage: $0 [-h hostname] [-o origins] [-r docker-registry]"
       exit 1
       ;;
   esac
@@ -32,6 +48,12 @@ done
 if [ -z "$HOSTNAME" ]; then
   HOSTNAME="localhost"
 fi
+if [ -z "$REGISTRY" ]; then
+  REGISTRY="docker.unsee.tech"
+fi
+if [ -z "$ORIGIN" ]; then
+  ORIGIN="localhost"
+fi
 
-docker compose build --build-arg BACKEND_URL=http://$HOSTNAME:8080
-FRONTEND_ORIGIN="http://$ORIGIN,http://localhost" docker compose up -d
+docker compose build --build-arg BACKEND_URL=http://$HOSTNAME:8080 DOCKER_REGISTRY=$REGISTRY
+FRONTEND_ORIGIN="http://$ORIGIN" docker compose up -d
