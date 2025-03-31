f240faf905238d757fd593d46047d11adcd7ed12
Bruce Jeaung <brucejeaung@gmail.com>
2025-03-31 21:59:23 +0800

    updated api spec

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # build images
 docker build server -t eis_server:latest --platform linux/amd64/v2
-docker build frontend -t eis_web:latest --platform linux/amd64/v2
+docker build --build-arg BACKEND_URL=http://192.168.1.2:8080 frontend -t eis_web:latest --platform linux/amd64/v2
 
 # pack images
 docker save -o eis-web.tar eis_web:latest
